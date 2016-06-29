456d6e170a8339957a8fe55396342ab5b28d878f
Nathan Venn <natevenn@gmail.com>
2016-06-28 21:17:21 -0400

    bob

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -6,9 +6,28 @@
 var Bob = function() {};
 
 Bob.prototype.hey = function(input) {
-//
-// YOUR CODE GOES HERE
-//
+    if (isAllCaps(input)) {
+      return 'Whoa, chill out!'
+    } else if (isQuestion(input)) {
+      return 'Sure.'
+    } else if (isEmpty(input)) {
+      return 'Fine. Be that way!'
+    } else {
+      return 'Whatever.'
+    }
 };
+
+function isAllCaps(str) {
+  var isNotValid = /^[0-9, ?]*$/.test(str);
+  return str === str.toUpperCase() && !isNotValid
+}
+
+function isQuestion(str) {
+  return str.slice(-1) === '?'
+}
+
+function isEmpty(str) {
+  return str.replace(/^\s+|\s+$/g, '').length == 0;
+}
 
 module.exports = Bob;
