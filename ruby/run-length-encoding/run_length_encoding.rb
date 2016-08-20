fbb038a57adbce1e9d6d7c6631858c296519df9a
Nathan Venn <natevenn@gmail.com>
2016-08-19 22:57:33 -0600

    run-length-encoding

--- ruby/run-length-encoding/run_length_encoding.rb
+++ ruby/run-length-encoding/run_length_encoding.rb
@@ -1,20 +1,44 @@
 class RunLengthEncoding
+ VERSION = 2
+
   def self.encode(input)
     input_array = input.chars
-    counter = 0
-    next_element = counter + 1
-    data_string = ""
-    while input_array[counter]
-      if input_array[next_element] != input_array[counter]
-        if counter == 0
-          data_string << input_array[counter]
-        else
-          data_string << input_array[counter] + counter
+    counter = 1
+    encoded_string = ''
+    input_array.each_with_index do |letter, index|
+      next_letter = input_array[index + 1]
+      if next_letter == letter
+        counter += 1
       else
-      counter += 1
-    require "pry"
-    binding.pry
+        if counter > 1
+          encoded_string << counter.to_s + letter
+        else
+          encoded_string << letter
+        end
+        counter = 1
+      end
     end
+    encoded_string
+  end
 
+  def self.decode(input)
+    decoded_string = ""
+    number = ''
+    index = 0
+    while index < input.length
+      current_element = input[index]
+      if current_element.to_i != 0
+        number << current_element
+      else
+        if number.empty?
+          decoded_string << current_element
+        else
+          decoded_string << current_element * number.to_i
+          number = ''
+        end
+      end
+      index += 1
+    end
+    decoded_string
   end
 end
